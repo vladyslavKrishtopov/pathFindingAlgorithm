14edbebbbf0949bb55b2f59bf424fa2ff957494f
Vladyslav Krishtopov <vlad@Vladyslavs-MacBook-Air.local>
2025-10-02 23:08:09 -0500

    Created path_finder.hpp/.cpp and declared the main class

---
+++ path_finder.cpp
@@ -0,0 +1,6 @@
+#include <path_finder.h>
+#include <iostream>
+
+void PathFinder::findPath(int** matrix, int path[][2]){
+
+}
---
+++ path_finder.h
@@ -0,0 +1,9 @@
+#ifndef PATH_FINDER_H
+#define PATH_FINDER_H
+
+class PathFinder{
+public:
+    static void findPath(int** matrix, int path[][2]);
+
+};
+#endif
